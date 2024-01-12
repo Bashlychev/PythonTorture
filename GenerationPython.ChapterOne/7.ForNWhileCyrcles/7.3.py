2c6369bd1af56ade52ecb2a0e8b2c07aa54b105f
Gleb <gbashlychev@gmail.com>
2024-01-12 12:23:49 +0500

    bis

--- GenerationPython.ChapterOne/7.ForNWhileCyrcles/7.3.py
+++ GenerationPython.ChapterOne/7.ForNWhileCyrcles/7.3.py
@@ -604,11 +604,14 @@
 # n = int(input())
 # max1=0
 # max2=0
-# for i in range (n):
-#     num=int(input())
-# max1 = max1 + i
-# max2 = max2 + i
-# print(max1+1)
+# for i in range (1,n+1):
+#     num = int(input())
+#     if num>max1:
+#         max2=max1
+#         max1=num
+#     elif num>max2:
+#         max2=num        
+# print(max1)
 # print(max2)
 
 # Only even numbers 🌶️
---
+++ GenerationPython.ChapterOne/9.Indexation/9.1.py
@@ -0,0 +1,35 @@
+# s = '01234567891011121314151617'
+# for i in range(0, len(s), 5):
+#     print(s[i], end='')
+
+# В столбик 1.
+# n = input()
+# for i in range(0,len(n),2):
+#     print(n[i])
+
+# В столбик 2
+# n = input()
+# for i in range(-1,-len(n)-1,-1):
+#     print(n[i])
+
+# ФИО
+# a,b,c = input(),input(),input()
+# print(b[0],a[0],c[0],sep='')
+
+# Цифра 1
+# n = input()
+# total = 0
+# for i in range (0,len(n)):
+#     total +=int(n[i]) 
+# print(total)
+
+# Цифра 2
+# n = input()
+# for i in range(len(n)):
+#     if n[i] in '123456789':
+#      flag = True
+#     break
+#     print('Цифра')
+#     else:print('Цифр нет')
+
+
